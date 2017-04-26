8585aa3970e89f69ba956d6578085ef16fa449ac
EhonAC7S <jordan.cesaro@telecomnancy.net>
2017-04-27 00:31:24 +0200

    Ajout backgroud sur le menu des cours et var dans class mises + mettre le bon git author

--- EnglishProject/app/src/main/java/com/example/englishproject/CoursActivity.java
+++ EnglishProject/app/src/main/java/com/example/englishproject/CoursActivity.java
@@ -2,8 +2,19 @@ package com.example.englishproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class CoursActivity extends AppCompatActivity {
+
+    Button cours1;
+    Button cours2;
+    Button cours3;
+    Button cours4;
+    Button cours5;
+    Button cours6;
+    Button nextPage;
+    Button previousPage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
