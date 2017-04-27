a2ddd53c3ceff5033273c06b4c1b73e95b471306
EhonAC7S <EhonAC7S>
2017-04-27 13:43:08 +0200

    Travail en cours sur CoursesActivity

--- EnglishProject/app/src/main/java/com/example/englishproject/menu/CoursActivity.java
+++ EnglishProject/app/src/main/java/com/example/englishproject/menu/CoursActivity.java
@@ -5,22 +5,46 @@ import android.os.Bundle;
 import android.widget.Button;
 
 import com.example.englishproject.R;
+import com.example.englishproject.database.BdCourses;
+import com.example.englishproject.database.DataElement.CoursesContents;
+
+import java.util.ArrayList;
 
 public class CoursActivity extends AppCompatActivity {
 
-    Button cours1;
-    Button cours2;
-    Button cours3;
-    Button cours4;
-    Button cours5;
-    Button cours6;
-    Button nextPage;
-    Button previousPage;
+    private ArrayList<CoursesContents> listeDesCours;
+    //private ArrayList<CoursesContents> listeDesCoursAffichés;
+    private int nbOfCoursDisplayed = 6; //Au nombre de 6 actuellement dans le layout
+    private Button cours1;
+    private Button cours2;
+    private Button cours3;
+    private Button cours4;
+    private Button cours5;
+    private Button cours6;
+    private Button nextPage;
+    private Button previousPage;
 
-
+ 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cours);
+
+        BdCourses bd = new BdCourses(this);
+        listeDesCours = bd.getAllCourses();
+        int coursNumber = listeDesCours.size();
+        int begin = 0;
+
+    }
+
+    public boolean canGoNext(int begin, int courseNumber) {
+        if (begin+nbOfCoursDisplayed < courseNumber) {
+            return true;
+        } else return false;
+    }
+    public boolean canGoPrevious(int begin) {
+        if (begin>=nbOfCoursDisplayed) {
+            return true;
+        } else return false;
     }
 }
